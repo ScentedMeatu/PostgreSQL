4d9cf1b6753733a7939a30e5396002443707d08c
Bharath K H <181883405+ScentedMeatu@users.noreply.github.com>
2024-12-16 10:30:11 +0530

    add DCL

---
+++ DCL.sql
@@ -0,0 +1,23 @@
+-- DCL(data control language)
+-- Used to control flow of data.
+
+-- Grant
+
+-- CREATE USER admin;
+-- GRANT CONNECT ON DATABASE trail TO admin;
+
+-- GRANT USAGE ON SCHEMA public TO admin;
+
+-- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+
+-- GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
+
+-- GRANT ALL PRIVILEGES ON DATABASE trail TO admin;
+
+-- ALTER USER admin CREATEDB;
+
+-- ALTER USER admin WITH SUPERUSER;
+
+-- Revoke
+
+-- REVOKE DELETE ON employee FROM admin;
