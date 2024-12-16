537230a4b70cc625e69136e18583bc4d00107b4c
Bharath K H <181883405+ScentedMeatu@users.noreply.github.com>
2024-12-16 11:22:25 +0530

    update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -3,28 +3,28 @@
 
 -- Create a table
 
--- CREATE TABLE employee (
---     emp_id SERIAL PRIMARY KEY,
---     first_name VARCHAR(50),
---     last_name VARCHAR(50),
---     department VARCHAR(50),
---     salary NUMERIC(10, 2),
---     hire_date DATE
--- );
+CREATE TABLE employee (
+    emp_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(50),
+    salary NUMERIC(10, 2),
+    hire_date DATE
+);
 
--- CREATE TABLE department (
---     dept_id SERIAL PRIMARY KEY,
---     dept_name VARCHAR(50)
--- );
+CREATE TABLE department (
+    dept_id SERIAL PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
 
 -- Alter a table
 
--- ALTER TABLE employee ADD COLUMN email VARCHAR(100);
+ALTER TABLE employee ADD COLUMN email VARCHAR(100);
 
 -- Truncate a table
 
--- TRUNCATE TABLE employee;
+TRUNCATE TABLE employee;
 
 -- Drop a table
 
--- DROP TABLE employee.
+DROP TABLE employee.
